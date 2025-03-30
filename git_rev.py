cb3d24170e6769a8d3a1550c6b8cc04317ba8ee7
Tomasz Wójcik <elroyski@gmail.com>
2025-03-30 17:24:28 +0200

    Dodano obsługę błędu przy braku tagów git

--- git_rev.py
+++ git_rev.py
@@ -1,5 +1,12 @@
 import subprocess
-# Get 0.0.0 version from latest Git tag
-tagcmd = "git describe --tags"
-version = subprocess.check_output(tagcmd, shell=True).decode().strip()
+import os
+
+try:
+    # Get 0.0.0 version from latest Git tag
+    tagcmd = "git describe --tags"
+    version = subprocess.check_output(tagcmd, shell=True).decode().strip()
+except subprocess.CalledProcessError:
+    # W przypadku błędu z git, użyj domyślnej wartości
+    version = "v0.0.0-dev"
+
 print(f'\'-D FIRMWAREVERSION="{version}"\'')
