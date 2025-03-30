7a997088888ee88de6c9b36d7f9eb6152779100c
Tomasz Wójcik <elroyski@gmail.com>
2025-03-30 21:52:36 +0200

    Naprawiono błąd deklaracji funkcji getStatus() - zmieniono zwracany typ na const char*

--- pio/lib/WiFiManagerKT/WiFiManagerKT.h
+++ pio/lib/WiFiManagerKT/WiFiManagerKT.h
@@ -40,6 +40,7 @@ const char HTTP_STYLE[] PROGMEM = "<style>.c{text-align: center;} div,input{padd
 const char HTTP_SCRIPT[] PROGMEM = "<script>function c(l){document.getElementById('s').value=l.innerText||l.textContent;document.getElementById('p').focus();}</script>";
 
 const char HTTP_HEAD_END[] PROGMEM = "</head><body><div style='text-align:left;display:inline-block;min-width:260px;'>";
+const char HTTP_HEADER_END[] PROGMEM = "</head><body><div style='text-align:left;display:inline-block;min-width:260px;'>";
 const char HTTP_PORTAL_OPTIONS[] PROGMEM = "<form action=\"/wifi\" method=\"get\"><button>Konfiguracja WiFi</button></form><br/><form action=\"/0wifi\" method=\"get\"><button>Konfiguracja WiFi (bez skanowania)</button></form><br/><form action=\"/i\" method=\"get\"><button>Informacje</button></form><br/><form action=\"/r\" method=\"post\"><button>Reset</button></form>";
 const char HTTP_ITEM[] PROGMEM = "<div><a href=\"#p\" onclick=\"c(this)\">{v}</a>&nbsp;<span class=\"q {i}\">{r}%</span></div>";
 const char JSON_ITEM[] PROGMEM = "{\"SSID\":\"{v}\", \"Encryption\":{i}, \"Quality\":\"{r}\"}";
@@ -153,7 +154,7 @@ private:
 
   void setupConfigPortal();
   void startWPS();
-  char *getStatus(int status);
+  const char *getStatus(int status);
 
   const char *_apName = "no-net";
   const char *_apPassword = NULL;
--- pio/src/iSpindel.cpp
+++ pio/src/iSpindel.cpp
@@ -1520,3 +1520,22 @@ void loop()
 {
   CONSOLELN(F("should never be here!"));
 }
+
+const char* getStatus(int status)
+{
+    switch (status)
+    {
+    case WL_IDLE_STATUS:
+        return "WL_IDLE_STATUS";
+    case WL_NO_SSID_AVAIL:
+        return "WL_NO_SSID_AVAIL";
+    case WL_CONNECTED:
+        return "WL_CONNECTED";
+    case WL_CONNECT_FAILED:
+        return "WL_CONNECT_FAILED";
+    case WL_DISCONNECTED:
+        return "WL_DISCONNECTED";
+    default:
+        return "UNKNOWN";
+    }
+}
